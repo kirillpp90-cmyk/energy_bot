ab5d875b49e6f72a542234ca182d93b2652689af
User <kirillpp90@gmail.com>
2026-05-30 16:44:19 +0300

    обновленноÐ about

--- handlers/about.py
+++ handlers/about.py
@@ -9,7 +9,8 @@ router = Router()
 async def about(message: Message):
     await message.answer(f'ℹ️ <b>Бот-калькулятор энергопотребления</b>\n\n'
                          '🔋 Версия 1.0\n\n'
-                         '👨‍💻 Автор: <a href="https://t.me/sherek008">Кирилл</a>\n\n'
+                         "Ссылка на репозиторий — </b> <a href='https://github.com/kirillpp90-cmyk/energy_bot'>energy_bot</a>\n\n"
+                         '👨‍💻 Автор: <a href="https://t.me/sherek008">Кирилл</a>, <a href="https://t.me/@zvsee">Дмитрий</a>\n\n'
                          '📋 <b>Доступные команды:</b>\n'
                          '/start - Начать работу\n'
                          '/calc - Калькулятор\n'
